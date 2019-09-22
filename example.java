35ee8e97ed851a96fb76301abbefb7f1f0e21b24
John Park <contact@jparksecurity.com>
2019-09-22 07:24:04 -0500

    example

---
+++ example.java
@@ -0,0 +1,10 @@
+class InputHandler {
+    public void handleInput();
+
+    // Methods to bind commands...
+
+    private Command buttonX_;
+    private Command buttonY_;
+    private Command buttonA_;
+    private Command buttonB_;
+}
